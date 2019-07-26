603b828e2504abbea1cd2c09bf36c722d2d40206
KYIV\Anton_Karpenko <apknov@gmail.com>
2019-07-26 18:31:01 +0300

    Change to TimeAPI Preparation

--- main.js
+++ main.js
@@ -3,20 +3,31 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
-import Counter from './Counter'
-import reducer from './reducers'
+// import Counter from './components/Counter'
+import App from './App'
+import reducer from './reducers/reducers'
+import rootSaga  from './middleware/sagas'
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
 
-const action = type => store.dispatch({type})
+sagaMiddleware.run(rootSaga)
+
+const action = type => store.dispatch({ type })
 
 function render() {
   ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+    // <Counter
+    //   value={store.getState()}
+    //   onIncrement={() => action('INCREMENT')}
+    //   onDecrement={() => action('DECREMENT')}
+    //   onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
+    <App />,
     document.getElementById('root')
   )
 }
